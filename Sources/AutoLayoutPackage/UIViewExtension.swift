b13a4663b75408e0e1e5b60826255a5d17ef8e68
Arthur Malulley <malulleybovo@wisc.edu>
2020-03-05 22:08:47 -0300

    Other - License - Include License in Source Files

--- Sources/AutoLayoutPackage/UIViewExtension.swift
+++ Sources/AutoLayoutPackage/UIViewExtension.swift
@@ -4,7 +4,32 @@
 //  Includes Auto Layout Helper Functions to All UIView
 //
 //  Created by malulleybovo on 03/03/20.
-//  Copyright © 2020 Arthur Malulley B. de O.
+//
+//  @author malulleybovo (since 2020)
+//  @license GNU General Public License v3.0
+//
+//  @licstart  The following is the entire license notice for the
+//   swift code in this page.
+//
+//  Copyright © 2020  Arthur Malulley B. de O.
+//
+//
+//  The swift code in this page is free software: you can
+//  redistribute it and/or modify it under the terms of the GNU
+//  General Public License (GNU GPL) as published by the Free Software
+//  Foundation, either version 3 of the License, or (at your option)
+//  any later version.  The code is distributed WITHOUT ANY WARRANTY;
+//  without even the implied warranty of MERCHANTABILITY or FITNESS
+//  FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+//
+//  As additional permission under GNU GPL version 3 section 7, you
+//  may distribute non-source (e.g., minimized or compacted) forms of
+//  that code without the copy of the GNU GPL normally required by
+//  section 4, provided you include this license notice and a URL
+//  through which recipients can access the Corresponding Source.
+//
+//  @licend  The above is the entire license notice
+//  for the swift code in this page.
 //
 
 import UIKit
